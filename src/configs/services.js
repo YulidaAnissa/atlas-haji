a7b4902445fcd7aea922ba98853ed8c913ed845c
YulidaAnissa <yulidaanissa@gmail.com>
2026-03-11 09:30:39 +0700

    update service backend

--- src/configs/services.js
+++ src/configs/services.js
@@ -1,4 +1,6 @@
-const SERVICE_BASE = process.env.NEXT_PUBLIC_SERVICE_BASE || 'https://atlas-haji-service.vercel.app';
+const SERVICE_BASE = process.env.NEXT_PUBLIC_SERVICE_BASE || 'http://atlas-haji-service.cloud';
+
+// https://atlas-haji-service.vercel.app
 
 const services = {
   SERVICE_BASE,
