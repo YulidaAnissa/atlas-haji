be1216c8616ad4ae40289845641a0d0fd903b908
YulidaAnissa <yulidaanissa@gmail.com>
2026-06-08 14:33:00 +0700

    validate login

--- src/components/forms/Login/validate.js
+++ src/components/forms/Login/validate.js
@@ -5,7 +5,6 @@ export default function validation(values) {
   return {
     username: validate(values.username, [
       { rule: 'required' },
-      { rule: 'isNumber', message: 'NIP harus berupa angka' },
     ]),
     password: validate(values.password, [
       { rule: 'required' },
