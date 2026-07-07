c68183857255882921b766771cf1933bb1664ed7
YulidaAnissa <yulidaanissa@gmail.com>
2026-07-07 17:57:41 +0700

    api

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  //baseURL: process.env.NEXT_PUBLIC_API_URL,
-  baseURL: "/api/proxy",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 function isTokenExpired(token) {
